2f3c79627ebd47eea3d915e92f32070c2c3e70a7
omnom-byte <131674821+omnom-byte@users.noreply.github.com>
2023-04-26 16:43:54 +0200

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -1,19 +1,27 @@
-python
-import numpy as np
+from typing import List
 import talib
+from config import LENGTH, OFFSET
 
 class Indicators:
-    def __init__(self):
-        pass
+    def __init__(self, period_weight: float, periods: List[int]):
+        self.period_weight = period_weight
+        self.periods = [int(p * period_weight) for p in periods]
+        self.window_size = max(self.periods) + OFFSET
+        self.prices = []
 
-    def rsi(self, close_values, period):
-        value = talib.RSI(close_values, timeperiod=period)[-1]
-        return value
+    def is_buy_signal(self) -> bool:
+        _, _, macd_hist = talib.MACDFIX(self.prices, signalperiod=self.periods[0], fastperiod=self.periods[1], slowperiod=self.periods[2])
+        if macd_hist[-1] <= macd_hist[-2] and macd_hist[-2] <= macd_hist[-3] and macd_hist[-3] <= macd_hist[-4] and macd_hist[-4] <= macd_hist[-5]:
+            return True
+        return False
 
-    def macd(self, close_values, period):
-        macd, signal, hist = talib.MACD(close_values, fastperiod=12, slowperiod=26, signalperiod=9)
-        return macd[-1], signal[-1]
+    def is_sell_signal(self) -> bool:
+        _, _, macd_hist = talib.MACDFIX(self.prices, signalperiod=self.periods[0], fastperiod=self.periods[1], slowperiod=self.periods[2])
+        if macd_hist[-1] >= macd_hist[-2] and macd_hist[-2] >= macd_hist[-3] and macd_hist[-3] >= macd_hist[-4] and macd_hist[-4] >= macd_hist[-5]:
+            return True
+        return False
 
-    def bollinger_bands(self, close_values, period):
-        upperband, middleband, lowerband = talib.BBANDS(close_values, timeperiod=period, nbdevup=2, nbdevdn=2)
-        return upperband[-1], middleband[-1], lowerband[-1]
+    def add_price(self, price: float) -> None:
+        if len(self.prices) == self.window_size:
+            self.prices.pop(0)
+        self.prices.append(price)
