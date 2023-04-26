92bb791b833c091fe3cc796830c8f4492ed8fcbb
omnom-byte <131674821+omnom-byte@users.noreply.github.com>
2023-04-26 16:49:58 +0200

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -1,21 +1,26 @@
-python
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import config
+import os
 
-class GoogleSheetsAPI:
-    def __init__(self, credentials_file, spreadsheet_name):
-        self.credentials_file = credentials_file
-        self.spreadsheet_name = spreadsheet_name
-        self.auth = self._authorize()
-        self.gs = self.auth.open(self.spreadsheet_name)
+import smartsheet
+from dotenv import load_dotenv
 
-    def _authorize(self):
-        credentials = ServiceAccountCredentials.from_json_keyfile_name(self.credentials_file, ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'])
-        auth = gspread.authorize(credentials)
-        return auth
+load_dotenv()
 
-    def update_spreadsheet(self, data):
-        sheet = self.gs.sheet1
-        row = [data['timestamp'], data['symbol'], data['action'], data['price'], data['quantity'], data['fee'], data['balance']]
-        sheet.append_row(row)
+class SheetAPI:
+    def __init__(self):
+        self.smart = smartsheet.Smartsheet(os.getenv('SMARTSHEET_TOKEN'))
+        self.sheet_id = int(os.getenv('SMARTSHEET_SHEET_ID'))
+
+    def get_row_ids(self) -> list:
+        sheet = self.smart.Sheets.get_sheet(self.sheet_id, page_size=1)
+        total_rows = sheet.total_row_count
+        paged_result = self.smart.Sheets.list_rows(self.sheet_id, page_size=total_rows)
+        row_ids = [row.id for row in paged_result.data]
+        return row_ids
+
+    def add_row(self, values: list) -> None:
+         new_row = smartsheet.models.Row()
+        new_row.to_top = True
+        new_row.cells.append(smartsheet.models.Cell(value=values[0]))
+        new_row.cells.append(smartsheet.models.Cell(value=values[1]))
+        new_row.cells.append(smartsheet.models.Cell(value=values[2]))
+        self.smart.Sheets.add_rows(self.sheet_id, [new_row])
