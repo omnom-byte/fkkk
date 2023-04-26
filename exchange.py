1c6daeeeac3db7f2577c738be422717be0db8d7b
omnom-byte <131674821+omnom-byte@users.noreply.github.com>
2023-04-26 14:08:09 +0200

    Update exchange.py

--- exchange.py
+++ exchange.py
@@ -1,1 +1,24 @@
+python
+from binance.client import Client
+import config
 
+class ExchangeAPI:
+
+    def __init__(self):
+        self.client = Client(config.API_KEY, config.API_SECRET)
+
+    def get_latest_price(self, symbol):
+        ticker = self.client.get_ticker(symbol=symbol)
+        return float(ticker['lastPrice'])
+
+    def get_candles(self, symbol, interval):
+        return self.client.get_klines(symbol=symbol, interval=interval)
+
+    def place_order(self, symbol, side, quantity, price):
+        self.client.create_order(
+            symbol=symbol,
+            side=side,
+            type='LIMIT',
+            timeInForce='GTC',
+            quantity=quantity,
+            price=price)
